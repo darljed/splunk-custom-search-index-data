49e0f02c5e95d409b1a8638c525faa103e2518cf
Darl Jed Matundan <djam.devteam@gmail.com>
2023-08-01 12:32:37 +0800

    working version of template

--- bin/index_data.py
+++ bin/index_data.py
@@ -3,48 +3,23 @@ from past.utils import old_div
 import splunk.Intersplunk 
 import base64
 
-import http.client
 from base64 import b64encode
 import json
-import datetime
 import sys
 import os
 import splunklib.client as client
 
 class Actions:
     def __init__(self,):
+        # initialize configurations 
+        # better if this can be moved to a configuration file 
         self.s_host="localhost"
         self.s_port="8089"
-        self.index="training_tracker"
-        self.log_index = "training_tracker_logs"
-        self.sourcetype="trainingtracker:json"
-
-
-    def logger(self,data):
-        service = self.splunk_connect(self.token)
-        target = service.indexes[self.log_index]
-        sourcetype="_json"
-
-        try:
-            if(type(data) == 'dict'):
-                data = str(json.dumps(data))
-            else:
-                data = str(data)
-            
-            data = {
-                "user_id":self.user_account,
-                "message":data
-                }
-
-            
-            target.submit(str(json.dumps(data)), sourcetype=sourcetype)
-
-            # print("Event indexed")
-        except Exception as error:
-            pass
+        self.index="custom_search_index_data_idx"
+        self.sourcetype="_json"
 
     def splunk_connect(self,sessionKey):
-
+        # create splunk service connection over http
         try:
             service = client.connect(
                 host=self.s_host,
@@ -54,86 +29,40 @@ class Actions:
             return service
 
         except Exception as error:
-            self.logger(error)
+            print(error)
 
-    def splunk_push(self,index, sourcetype,sessionKey,data):
+    def splunk_push(self,index, sourcetype,sessionKey,data): # this function saves data to the provided argument "index" when invoked
         service = self.splunk_connect(sessionKey)
         target = service.indexes[index]
         sourcetype=sourcetype
 
         try:
-            # data['timestamp'] = self.get_timestamp()
-            # self.logger(data.get('_time'))
-            target.submit(str(json.dumps(data)), sourcetype=sourcetype)
-            self.logger("Successfully updated training tracker record")
-            # print("Event indexed")
+            target.submit(str(data), sourcetype=sourcetype) # triggers submission and indexing of data
             return True
         except Exception as error:
-            self.logger("Failed to log training tracker record. Error:"+str(error))
-            # print(error)
-            return error
+            return f'Event not indexed. {error}'
 
-
-
-    def get_timestamp(self,epoch):
-        # self.logger(f'Epoch value: {epoch}')
-        timestamp = datetime.datetime.fromtimestamp(int(epoch.split('.')[0])).strftime("%Y-%m-%dT%H:%M:%S")
-        # timestamp = int(epoch.split('.')[0])
-        # self.logger(f"Generated timestamp: {timestamp}")
-        return timestamp
 
     def add_record(self):
         try:
             results, dummyresults, settings = splunk.Intersplunk.getOrganizedResults()
-            self.token = settings.get("sessionKey", None)
-            self.user_account = settings.get('owner')
+            self.token = settings.get("sessionKey", None) # capture session key from user's auth details
             for result in results:
-                # self.logger(f"date value from organizeddata {result.get('id')}")
-                data = {
-                    "trainingslug": result.get('trainingslug'),
-                    "id": result.get('id'),
-                    "status": result.get('status'),
-                    "owner": settings.get('owner'),
-                    "timestamp": self.get_timestamp(result.get('date')),
-                    "imgfilename": result.get('filename') if result.get('status')=="Completed" else "None"
-                }
                 
-                post = self.splunk_push(self.index,self.sourcetype,settings.get("sessionKey", None),data)
-                ## Genrate image
-                if(result.get('status') == "Completed"):
-                    self.saveImageBlob(result.get('imgblob'),result.get('filename'))
-                result['result'] = str(post)
-                splunk.Intersplunk.outputResults(results)
+                # you can modify and add more fields here that will be added to the data when indexed
+                result['Greetings'] = "Hello World!"
+                # save data to index / index data
+                post = self.splunk_push(self.index,self.sourcetype,self.token,result)
+                # all changes to 'result' dict after this will not be indexed
+                result['is_indexed'] = str(post)
+                # result['sessionKey'] = self.token
+            
+            # return the results from the search in search page
+            splunk.Intersplunk.outputResults(results)
                 
-            # f = open('test.txt','w')
-            # f.write(str(settings))
                 
         except Exception as e2:
-            self.logger(f"Unable to save result. {e2}")
             splunk.Intersplunk.generateErrorResults("Error '%s'. %s" % (e2))
-        # for result in results:
-        #     result['dummyresults'] = dummyresults
-        #     result['settings'] = settings
-        #     result['data'] = data
-        #     result['code'] = 200
-        #     result['sessionKey'] = settings.get("sessionKey", None)
-        
-        # self.logger(result)
-        # print("Test")
-
-    def saveImageBlob(self,blob,filename):
-        assetsdir = os.path.abspath(os.path.join(os.path.dirname(__file__),'../','appserver','static','training_tracker_proofimages'))
-        if(not os.path.exists(assetsdir)):
-            os.makedirs(assetsdir)
-        
-        imagefile = blob
-        # imagefile = imagefile.replace("proofimage=","")
-        image = imagefile.split(',')[-1]
-        binary_data = image.encode()
-
-        fd = open(os.path.join(assetsdir,filename), 'wb')
-        fd.write(base64.decodebytes(binary_data))
-        fd.close()
 
 def main():
     e = Actions()
